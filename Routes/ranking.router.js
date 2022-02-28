b4ed28ea67d288f63e60daf0d50d1fbde0c28339
ioserakbar <ioserakbar@gmail.com>
2022-02-27 22:14:33 -0600

    ranking fixed

--- Routes/ranking.router.js
+++ Routes/ranking.router.js
@@ -1,9 +1,9 @@
 const express= require('express');
 const router = express.Router();
-const Service = require('../Services/publicationMultimedia.service');
+const Service = require('../Services/ranking.service');
 const service = new Service();
 const validatorHandler = require('./../Middlewares/validator.handler')
-const { createPublicationSchema, updatePublicationSchema, getValidPublication } = require('../Schemas/publicationMultimedia.schema');
+const { createPublicationSchema, updatePublicationSchema, getValidPublication } = require('../Schemas/ranking.schema');
 
 
 //GET ALL PRODUCTS
